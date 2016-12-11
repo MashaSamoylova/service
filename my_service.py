8c94a5f0c0ef74aaa340d55b8df8ab6ace356e9d
MashaSamoylova <mashanedyak@gmail.com>
2016-12-11 14:41:34 +0600

    added some checks and extra forms in the registration

--- my_service.py
+++ my_service.py
@@ -6,7 +6,7 @@ import os
 users_db = sqlite3.connect('users.db')
 cur = users_db.cursor()
 #cur.execute('''CREATE TABLE users(id INTEGER PRIMARY KEY, login VARCHAR(100),
-#password VARCHAR(100), secret VARCHAR(100), avatar VARCHAR(100))''')
+#password VARCHAR(100), age INTEGER, capacity VARCHAR(100), secret VARCHAR(100), avatar VARCHAR(100))''')
 #users_db.commit()
 
 #show all requests
@@ -39,13 +39,26 @@ class LoginHandler(BaseHandler):
 
 class RegistrationHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("./templates/registration.html")
+        self.render("./templates/registration.html", msg="")
 
     def post(self):
         print self.get_body_argument('login')
-    #    cur.execute() DJNNNJNHVHGKJKLHJFGDHHGJKL:"LKJHGFDGHJKL:"LKJHGFD
-        cur.execute('''INSERT INTO users (id, login, password, secret, avatar) VALUES(NULL,?,?,?, "./static/images/mprofile.png")''',
-        (self.get_body_argument('login'), self.get_body_argument('password'), self.get_body_argument('secret_place')))
+        cur.execute('''SELECT COUNT(*) FROM users WHERE login=?''', (self.get_body_argument('login'),))
+        for row in cur:
+            if row[0]:
+                self.render("./templates/registration.html", msg = "This login is busy")
+        try:
+            int_age = int(self.get_body_argument('age'))
+        except:
+            self.render("./templates/registration.html", msg = "Age should be numeric")
+            return
+        cur.execute('''INSERT INTO users (id, login, password, age, capacity, secret, avatar)
+                    VALUES(NULL,?,?,?,?,?, "./static/images/mprofile.png")''',
+                    (self.get_body_argument('login'),
+                    self.get_body_argument('password'),
+                    int_age,
+                    self.get_body_argument('capacity'),
+                    self.get_body_argument('secret_place')))
         users_db.commit()
         self.redirect('/login')
 
@@ -54,13 +67,19 @@ class MyprofileHandler(BaseHandler):
         name = tornado.escape.xhtml_escape(self.current_user)
         cur.execute('''SELECT * FROM users WHERE login=?''', (name,))
         for row in cur:
-            self.render("./templates/template_user_page.html", title=name, photo=row[4])
+            self.render("./templates/template_user_page.html",
+                        title=name,
+                        login=name,
+                        age=row[3],
+                        capacity=row[4],
+                        secret_place=row[5],
+                        photo=row[6])
 
 class PhotoHandler(BaseHandler):
     def post(self):
         name = tornado.escape.xhtml_escape(self.current_user)
         photo = self.request.files['photo'][0]['body']
-        with open("./static/images/photo_users" + name + ".jpg", 'w') as f:
+        with open("./static/images/photo_users/" + name + ".jpg", 'w') as f:
             f.write(photo)
             f.close()
             cur.execute('''UPDATE users SET avatar=? WHERE login=?''', ("./static/images/photo_users/" + name + ".jpg", name))
@@ -71,7 +90,6 @@ class LogoutHandler(BaseHandler):
     def get(self):
         self.clear_cookie("user")
         self.redirect('/')
-
 
 settings = {
     "templates_path": os.path.join(os.path.dirname("./templates"), "templates"),
@@ -87,7 +105,6 @@ def make_app():
     (r"/myprofile", MyprofileHandler),
     (r"/photo", PhotoHandler),
     (r"/logout", LogoutHandler),
-#    (r"/login", LogHandler),
     ], **settings)
 
 if __name__=="__main__":
